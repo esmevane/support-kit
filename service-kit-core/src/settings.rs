cfdb56c02f72bdddce4f8f0d42fa44b405f3bc1c
Joseph McCormick <esmevane@gmail.com>
2024-07-08 16:35:45 -0500

    @wip: defaults for configuration.

--- service-kit-core/src/settings.rs
+++ service-kit-core/src/settings.rs
@@ -149,38 +149,6 @@ impl Settings {
 
     #[tracing::instrument(level = "debug", skip(self))]
     pub fn storage_path(&self) -> PathBuf {
-        tracing::info!("Getting storage path");
-        let path = match &self.config.storage {
-            Some(storage) => storage.path.clone(),
-            None => {
-                // use directories to get a default data directory in user's config path
-                match dirs::config_local_dir() {
-                    Some(mut path) => {
-                        path.push(APP_NAME.to_lowercase());
-                        path.push("storage.db");
-                        path
-                    }
-                    None => {
-                        // otherwise we start in a temp directory
-                        std::env::temp_dir().join("storage.db")
-                    }
-                }
-            }
-        };
-
-        tracing::info!("Using storage path: {}", path.display());
-
-        // ensure the file and path exist
-        if let Some(parent) = path.parent() {
-            tracing::info!("Ensuring storage path exists: {:?}", parent);
-            std::fs::create_dir_all(parent).expect("Unable to create storage directory");
-        }
-
-        if std::fs::metadata(&path).is_err() {
-            tracing::info!("Ensuring storage file exists: {:?}", path);
-            std::fs::File::create(&path).expect("Unable to create storage file");
-        }
-
-        path
+        self.config.storage.path.clone()
     }
 }
--- service-kit-core/src/settings/configuration.rs
+++ service-kit-core/src/settings/configuration.rs
@@ -1,27 +1,53 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
+use crate::APP_NAME;
+
 #[derive(Clone, Default, Debug, Deserialize)]
 #[serde(default)]
 pub struct Configuration {
-    pub db: Option<Database>,
-    pub storage: Option<Storage>,
+    pub db: Database,
+    pub storage: Storage,
     pub logging: Logging,
 }
-
-// impl Default for Configuration {
-//     fn default() -> Self {
-//         Self {
-//             db: None,
-//             storage: None,
-//             ..Default::default()
-//         }
-//     }
-// }
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct Storage {
     pub path: PathBuf,
+}
+
+impl Default for Storage {
+    fn default() -> Self {
+        // use directories to get a default data directory in user's config path
+        let path = match dirs::config_local_dir() {
+            Some(mut path) => {
+                path.push(APP_NAME.to_lowercase());
+                path.push("storage.db");
+                path
+            }
+            None => {
+                // otherwise we start in a temp directory
+                std::env::temp_dir()
+                    .join(APP_NAME.to_lowercase())
+                    .join("storage.db")
+            }
+        };
+
+        tracing::debug!("Using storage path: {}", path.display());
+
+        // ensure the file and path exist
+        if let Some(parent) = path.parent() {
+            tracing::debug!("Ensuring storage path exists: {:?}", parent);
+            std::fs::create_dir_all(parent).expect("Unable to create storage directory");
+        }
+
+        if std::fs::metadata(&path).is_err() {
+            tracing::debug!("Ensuring storage file exists: {:?}", path);
+            std::fs::File::create(&path).expect("Unable to create storage file");
+        }
+
+        Self { path }
+    }
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -33,6 +59,18 @@ pub struct Database {
     database: Option<String>,
 }
 
+impl Default for Database {
+    fn default() -> Self {
+        Self {
+            host: "localhost".to_string(),
+            port: 3306,
+            user: "root".to_string(),
+            password: "password".to_string(),
+            database: None,
+        }
+    }
+}
+
 impl std::fmt::Display for Database {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -42,7 +80,7 @@ impl std::fmt::Display for Database {
             self.password,
             self.host,
             self.port,
-            self.database.as_deref().unwrap_or("acore_world"),
+            self.database.as_deref().unwrap_or(&APP_NAME),
         )
     }
 }
