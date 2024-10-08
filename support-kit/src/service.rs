2f41df8d09f2422fdbdeaee3876f3ccbfa45e096
Joseph McCormick <esmevane@gmail.com>
2024-10-08 04:45:10 -0500

    @wip

--- support-kit/src/service.rs
+++ support-kit/src/service.rs
@@ -13,69 +13,6 @@ pub use service_control_error::ServiceControlError;
 pub use service_label::ServiceLabel;
 
 #[test]
-fn service_args() -> Result<(), Box<dyn std::error::Error>> {
-    use clap::Parser;
-    use ServiceCommand::*;
-
-    let expectations = [
-        ("app", None),
-        ("app install", Some(Install)),
-        ("app start", Some(Start)),
-        ("app stop", Some(Stop)),
-        ("app uninstall", Some(Uninstall)),
-    ];
-
-    for (input, expected) in expectations {
-        let cli = ServiceArgs::try_parse_from(input.split_whitespace())?;
-
-        assert_eq!(cli.operation, expected);
-    }
-
-    let expectations = [
-        ("app", "support-kit"),
-        ("app -n app-name", "app-name"),
-        ("app --name app-name", "app-name"),
-    ];
-
-    for (input, expected) in expectations {
-        let cli = ServiceArgs::try_parse_from(input.split_whitespace())?;
-
-        assert_eq!(cli.label.unwrap_or_default(), expected.into());
-    }
-
-    figment::Jail::expect_with(|jail| {
-        jail.set_env("CARGO_PKG_NAME", "consumer-package");
-
-        assert_eq!(service_label::get_runtime_name(), "consumer-package");
-
-        let expectations = [
-            ("app", "consumer-package"),
-            ("app -n app-name", "app-name"),
-            ("app --name app-name", "app-name"),
-        ];
-
-        for (input, expected) in expectations {
-            let cli =
-                ServiceArgs::try_parse_from(input.split_whitespace()).expect("failed to parse");
-
-            assert_eq!(cli.label.unwrap_or_default(), expected.into());
-        }
-
-        Ok(())
-    });
-
-    let expectations = [("app", false), ("app --system", true)];
-
-    for (input, expected) in expectations {
-        let cli = ServiceArgs::try_parse_from(input.split_whitespace())?;
-
-        assert_eq!(cli.system, expected);
-    }
-
-    Ok(())
-}
-
-#[test]
 fn service_config() -> Result<(), Box<dyn std::error::Error>> {
     // let expectations = [
     //     ("app", "support-kit"),
--- support-kit/src/service/service_args.rs
+++ support-kit/src/service/service_args.rs
@@ -44,3 +44,85 @@ impl From<ServiceCommand> for ServiceArgs {
         Self::builder().operation(command).build()
     }
 }
+
+#[test]
+fn operations() -> Result<(), Box<dyn std::error::Error>> {
+    use clap::Parser;
+    use ServiceCommand::*;
+
+    let expectations = [
+        ("app", None),
+        ("app install", Some(Install)),
+        ("app start", Some(Start)),
+        ("app stop", Some(Stop)),
+        ("app uninstall", Some(Uninstall)),
+    ];
+
+    for (input, expected) in expectations {
+        let cli = ServiceArgs::try_parse_from(input.split_whitespace())?;
+
+        assert_eq!(cli.operation, expected);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn setting_labels() -> Result<(), Box<dyn std::error::Error>> {
+    use clap::Parser;
+
+    let expectations = [
+        ("app", "support-kit"),
+        ("app -n app-name", "app-name"),
+        ("app --name app-name", "app-name"),
+    ];
+
+    for (input, expected) in expectations {
+        let cli = ServiceArgs::try_parse_from(input.split_whitespace())?;
+
+        assert_eq!(cli.label.unwrap_or_default(), expected.into());
+    }
+
+    Ok(())
+}
+
+#[test]
+fn setting_system_flag() -> Result<(), Box<dyn std::error::Error>> {
+    use clap::Parser;
+
+    let expectations = [("app", false), ("app --system", true)];
+
+    for (input, expected) in expectations {
+        let cli = ServiceArgs::try_parse_from(input.split_whitespace())?;
+
+        assert_eq!(cli.system, expected);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn reading_cargo_env_for_defaults() -> Result<(), Box<dyn std::error::Error>> {
+    use clap::Parser;
+
+    figment::Jail::expect_with(|jail| {
+        jail.set_env("CARGO_PKG_NAME", "consumer-package");
+
+        let expectations = [
+            ("app", "consumer-package"),
+            ("app -n app-name", "app-name"),
+            ("app --name app-name", "app-name"),
+        ];
+
+        for (input, expected) in expectations {
+            let cli =
+                ServiceArgs::try_parse_from(input.split_whitespace()).expect("failed to parse");
+
+            assert_eq!(cli.label.unwrap_or_default(), expected.into());
+        }
+
+        Ok(())
+    });
+
+    Ok(())
+}
