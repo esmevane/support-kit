7716fcaf9f69acecf5a9d4cca91559620337df12
Joseph McCormick <esmevane@gmail.com>
2024-10-09 20:10:52 -0500

    Move 'structures' into a flat file.

--- support-kit/src/structures.rs
+++ support-kit/src/structures.rs
@@ -1,5 +1,38 @@
-mod config_or_preset;
-mod one_or_many;
+mod config_or_preset {
+    use serde::{Deserialize, Serialize};
+
+    // Either a configuration (struct) or a preset (enum).
+    #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+    #[serde(untagged)]
+    pub enum ConfigOrPreset<Config, Preset> {
+        Config(Config),
+        Preset(Preset),
+    }
+}
+
+mod one_or_many {
+
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+    #[serde(untagged)]
+    pub enum OneOrMany<Contents> {
+        Many(Vec<Contents>),
+        One(Contents),
+    }
+
+    impl<Contents> From<Vec<Contents>> for OneOrMany<Contents> {
+        fn from(value: Vec<Contents>) -> Self {
+            Self::Many(value)
+        }
+    }
+
+    impl<Contents> From<Contents> for OneOrMany<Contents> {
+        fn from(value: Contents) -> Self {
+            Self::One(value)
+        }
+    }
+}
 
 pub use config_or_preset::ConfigOrPreset;
 pub use one_or_many::OneOrMany;
--- support-kit/src/structures/config_or_preset.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-// Either a configuration (struct) or a preset (enum).
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
-#[serde(untagged)]
-pub enum ConfigOrPreset<Config, Preset> {
-    Config(Config),
-    Preset(Preset),
-}
