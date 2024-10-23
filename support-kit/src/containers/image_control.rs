e0154ebdc05a39888750758d7b79e8a606a00235
Joseph McCormick <esmevane@gmail.com>
2024-10-23 09:53:41 -0500

    fix: Container build goof
    I had a .. instead of . in the Docker build context so that I could test
    out local dependencies, and wouldn't you know it, I forgot to remove it.
    This fixes that specific issue and gets us some tracing so debugging it
    next time is a little less opaque.

--- support-kit/src/containers/image_control.rs
+++ support-kit/src/containers/image_control.rs
@@ -9,6 +9,7 @@ pub struct ImageControl {
 }
 
 impl ImageControl {
+    #[tracing::instrument(skip(self), level = "trace")]
     fn descriptor(&self) -> String {
         format!(
             "{repo}/{namespace}/{name}:{label}",
@@ -19,6 +20,7 @@ impl ImageControl {
         )
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     fn name(&self) -> String {
         format!(
             "{namespace}-{name}-deployment",
@@ -27,48 +29,58 @@ impl ImageControl {
         )
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     pub fn setup_config_volume(&self) -> crate::Result<OpsProcess> {
-        Ok(format!(
+        to_image_op(format!(
             "docker volume create {namespace}-{name}-config",
             name = self.image.name,
             namespace = self.image.namespace
-        )
-        .try_into()?)
+        ))
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     pub fn kill_all(&self) -> crate::Result<OpsProcess> {
         let container_ids = format!("docker ps -qf name={name}", name = self.name());
 
-        Ok(format!("docker kill $({container_ids})").try_into()?)
+        to_image_op(format!("docker kill $({container_ids})"))
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     pub fn push(&self) -> crate::Result<OpsProcess> {
-        Ok(format!("docker push {descriptor}", descriptor = self.descriptor()).try_into()?)
+        to_image_op(format!(
+            "docker push {descriptor}",
+            descriptor = self.descriptor()
+        ))
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     pub fn build(&self) -> crate::Result<OpsProcess> {
         let label = format!(
             "org.opencontainers.image.source={repo}",
             repo = self.image.repo
         );
 
-        Ok(format!(
+        to_image_op(format!(
             "docker build \
             -f {definition} \
             --label {label} \
             -t {descriptor} ..",
             definition = self.image.definition,
             descriptor = self.descriptor(),
-        )
-        .try_into()?)
+        ))
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     pub fn pull(&self) -> crate::Result<OpsProcess> {
-        Ok(format!("docker pull {descriptor}", descriptor = self.descriptor()).try_into()?)
+        to_image_op(format!(
+            "docker pull {descriptor}",
+            descriptor = self.descriptor()
+        ))
     }
 
+    #[tracing::instrument(skip(self), level = "trace")]
     pub fn start(&self) -> crate::Result<OpsProcess> {
-        Ok(format!(
+        to_image_op(format!(
             "docker run --rm \
             -p 443:{port} \
             --mount source=certs,target=/certs \
@@ -76,7 +88,15 @@ impl ImageControl {
             {descriptor}",
             descriptor = self.descriptor(),
             port = self.config.server.port,
-        )
-        .try_into()?)
+        ))
     }
+}
+
+#[tracing::instrument(skip(operation), level = "trace")]
+fn to_image_op<T: Into<String>>(operation: T) -> crate::Result<OpsProcess> {
+    let operation = operation.into();
+
+    tracing::trace!(operation = ?operation, "converting to operation");
+
+    Ok(operation.try_into()?)
 }
